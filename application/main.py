125fdc8458ca7e88de634f18c84ad153075ff089
Justyn Roberts <justynroberts@gmail.com>
2023-03-15 16:01:43 +0000

    Big Commit. Redo UI & Logic! Well done!

--- application/main.py
+++ application/main.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
 import requests
 import json
 app = Flask(__name__) 
 
 # For testing---------------------------------------
 # Temporary - Enter API Token here for testing
-API_KEY = "u+ssxrNVefysigXJyEHw"
-APIKEY_REGION = "US"
+#API_KEY = "u+ssxrNVefysigXJyEHw"
+#APIKEY_REGION = "US"
 # or swap out for env variables
 
 #API_TOKEN = os.environ['APIKEY']
@@ -17,20 +17,27 @@ SERVICES = []
 RUNNERS = []
 with open("configuration/tasks.json", "r") as f:
     TASKS = json.load(f)
-payload = ""
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Token token=" + API_KEY,
-}
-if APIKEY_REGION == "EU":
-    services_url = "https://api.eu.pagerduty.com/services"
-    automation_url = "https://api.eu.pagerduty.com/automation_actions/actions"
-    runners_url = "https://api.eu.pagerduty.com/automation_actions/runners"
-else:
-    services_url = "https://api.pagerduty.com/services"
-    automation_url = "https://api.pagerduty.com/automation_actions/actions"
-    runners_url = "https://api.pagerduty.com/automation_actions/runners"
+
 # Main create AA task
+
+def updateservices():
+  services_response = requests.request(
+            "GET", services_url, data=payload, headers=headers
+        )
+  services_data = services_response.json()
+  print (services_data)
+  runner_response = requests.request(
+            "GET", runners_url, data=payload, headers=headers
+        )
+  runners_data = runner_response.json()
+  RUNNERS = []
+  SERVICES = []
+  for service in services_data["services"]:
+      SERVICES.append({"id": service["id"], "name": service["name"]})
+      for runners in runners_data["runners"]:
+        if runners["runner_type"] == "sidecar":
+          RUNNERS.append({"id": runners["id"], "name": runners["name"]})
+
 def create_action(selected_services,selected_runner,selected_tasks):
     selected_tasks = [int(id) for id in selected_tasks]
     processed_task = [item for item in TASKS if item['id'] in selected_tasks]
@@ -50,12 +57,51 @@ def create_action(selected_services,selected_runner,selected_tasks):
         response = requests.request("POST", automation_url, json=aa_payload, headers=headers)
     return ()
 
+@app.route('/update_api_key_and_region', methods=['POST'])
+def update_api_key_and_region():
+    API_KEY = request.form.get('api_key')
+    APIKEY_REGION = request.form.get('region')
+    if APIKEY_REGION == "EU":
+        services_url = "https://api.eu.pagerduty.com/services"
+        automation_url = "https://api.eu.pagerduty.com/automation_actions/actions"
+        runners_url = "https://api.eu.pagerduty.com/automation_actions/runners"
+    else:
+        services_url = "https://api.pagerduty.com/services"
+        automation_url = "https://api.pagerduty.com/automation_actions/actions"
+        runners_url = "https://api.pagerduty.com/automation_actions/runners"
+
+    payload = ""
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Token token=" + API_KEY,}
+
+    RUNNERS = []
+    SERVICES = []
+
+    services_response = requests.request(
+                "GET", services_url, data=payload, headers=headers
+            )
+    services_data = services_response.json()
+    runner_response = requests.request(
+                "GET", runners_url, data=payload, headers=headers
+            )
+    runners_data = runner_response.json()
+    for service in services_data["services"]:
+        SERVICES.append({"id": service["id"], "name": service["name"]})
+
+    for runners in runners_data["runners"]:
+        if runners["runner_type"] == "sidecar":
+          RUNNERS.append({"id": runners["id"], "name": runners["name"]})
+
+    print(SERVICES)
+    return jsonify({
+        'services': SERVICES,
+        'runners': RUNNERS,
+    })
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
     if request.method == "POST":
-        # Get the selected services and tasks
-        #API_TOKEN = request.form['text_value'] 
         selected_services = request.form.getlist("services")
         selected_tasks = request.form.getlist("tasks")
         selected_runners = request.form.getlist("runners")  
@@ -66,23 +112,6 @@ def index():
         # Redirect to the index page
         return redirect(url_for("index", echo_message=echo_message))
     else:
-        services_response = requests.request(
-            "GET", services_url, data=payload, headers=headers
-        )
-        services_data = services_response.json()
-        runner_response = requests.request(
-            "GET", runners_url, data=payload, headers=headers
-        )
-        runners_data = runner_response.json()
-        RUNNERS = []
-        SERVICES = []
-        for service in services_data["services"]:
-            SERVICES.append({"id": service["id"], "name": service["name"]})
-
-        for runners in runners_data["runners"]:
-            if runners["runner_type"] == "sidecar":
-                RUNNERS.append({"id": runners["id"], "name": runners["name"]})
-
         echo_message = request.args.get("echo_message", "")
         # Render the template with the services and tasks data and popup message
         return render_template(
@@ -93,4 +122,6 @@ def index():
             echo_message=echo_message,
         )
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run(port=5001,debug=True)
+
